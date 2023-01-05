52da2e60032be669b83b7e8e9de5d4891360f310
Saiyam Pathak <saiyam911@gmail.com>
2023-01-05 17:41:49 +0530

    Update promethues_setup.sh

--- promethues_setup.sh
+++ promethues_setup.sh
@@ -12,6 +12,35 @@ sudo mv consoles/ console_libraries/ /etc/prometheus/
 cd $HOME
 for i in rules rules.d files_sd; do sudo chown -R prometheus:prometheus /etc/prometheus/${i}; done
 for i in rules rules.d files_sd; do sudo chmod -R 775 /etc/prometheus/${i}; done
+
+sudo tee /etc/systemd/system/prometheus.service<<EOF
+[Unit]
+Description=Prometheus
+Documentation=https://prometheus.io/docs/introduction/overview/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP \$MAINPID
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/var/lib/prometheus \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.external-url=
+
+SyslogIdentifier=prometheus
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
 sudo chown -R prometheus:prometheus /var/lib/prometheus/
 sudo systemctl daemon-reload
 sudo systemctl start prometheus
